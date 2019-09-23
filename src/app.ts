3f664f9c33c4e0f784a34413723c7a63417df99d
Michael Cohen <michael@fatastronauts.com>
2019-09-22 20:35:20 -0500

    removed unused files

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,3 @@
-import Display from './display';
+function main() {}
 
-let display: Display;
-
-function main(){
-    display = new Display(__CONFIG__.pins.sclPin, __CONFIG__.pins.sdaPin, function() {
-        display.update();
-    });
-
-    setInterval(() => {
-        display.drawTime();
-    }, 1000);
-}
-
-E.on('init', main);
+E.on("init", main);
--- src/fonts/font4x4Numeric.js
+++
@@ -1,4 +0,0 @@
-var font4x4Numeric = atob("/b+f8bvdmd/m/9279VeI/z+/6q8=");
-Graphics.prototype.setFont4x4Numeric = function() {
-    this.setFontCustom(font4x4Numeric, "0".charCodeAt(0), 4, 4);
-}
